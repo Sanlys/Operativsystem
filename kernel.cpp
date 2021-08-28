5ee7f3917af17c11c474572bc71c408b34e3b46f
Sander <slysakermoen@gmail.com>
2021-08-28 17:59:46 +0200

    Created kernel.cpp
    Added print function, writes to memory from 0xb8000, where the GPU reads from

--- kernel.cpp
+++ kernel.cpp
@@ -1,4 +1,12 @@
+void print(char* str) {
+	unsigned short* VideoMemory = (unsigned short*)0xb8000;
+
+	for(int i = 0; str[i] != '\0'; ++i) {
+		VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+	}
+}
+
 void kernelMain(void* multiboot_structure, unsigned int magicnumber) {
-	printf("Kernel test");
+	print("Kernel test");
 	while(1);
 }
