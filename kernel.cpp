53097b464d885d9fe6232d5c0f7e3873421931ac
Sander <slysakermoen@gmail.com>
2021-08-29 13:46:33 +0200

    Set specific data types for hardware interfacing

--- kernel.cpp
+++ kernel.cpp
@@ -1,5 +1,7 @@
+#include "types.h"
+
 void print(char* str) {
-	unsigned short* VideoMemory = (unsigned short*)0xb8000;
+	static uint16_t* VideoMemory = (uint16_t*)0xb8000;
 
 	for(int i = 0; str[i] != '\0'; ++i) {
 		VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
@@ -15,7 +17,7 @@ extern "C" void callConstructors() {
 	}
 }
 
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber) {
+extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber) {
 	print("Kernel test");
 	while(1);
 }
