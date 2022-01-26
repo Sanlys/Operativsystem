f70298074bdd910f71e7b99eb37c407c55049f9d
Sanlys <49021092+Sanlys@users.noreply.github.com>
2022-01-26 09:41:50 +0100

    gdt og fancy print

--- kernel.cpp
+++ kernel.cpp
@@ -1,10 +1,11 @@
 #include "types.h"
 
-void print(char* str) {
+void print(char* str, int clr, int x, int y) {
 	static uint16_t* VideoMemory = (uint16_t*)0xb8000;
-
+	
+	int offset = y * 80 + x;
 	for(int i = 0; str[i] != '\0'; ++i) {
-		VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+		VideoMemory[i+offset] = (VideoMemory[i+offset] & clr) | str[i];
 	}
 }
 
@@ -18,6 +19,9 @@ extern "C" void callConstructors() {
 }
 
 extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber) {
-	print("Kernel test");
+	print("Kernel test", 0xFF00, 0, 0);
+	print("Hei", 0x5500, 0, 1);
+	print("Fler", 0xFF00, 0, 2);
+	print("farget tekst", 0x5500, 4, 2);
 	while(1);
 }
