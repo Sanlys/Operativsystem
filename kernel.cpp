048ffbfe254f735354d4f445e8828ae846bf91e1
Sanlys <49021092+Sanlys@users.noreply.github.com>
2022-01-28 12:58:46 +0100

    starta med ports

--- kernel.cpp
+++ kernel.cpp
@@ -24,7 +24,8 @@ extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber) {
 	print("Hei", 0x5500, 0, 1);
 	print("Fler", 0xFF00, 0, 2);
 	print("farget tekst", 0x5500, 4, 2);
-	
+	print("> ", 0xFF00, 0, 4);
+
 	GlobalDescriptorTable gdt;
 	
 	while(1);
